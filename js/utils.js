352b96ed359ddc4309187fcd48af3805a41e8c15
Cold Meson 06 <fernando.devmines@gmail.com>
2017-05-30 03:32:11 -0300

    Initial site structure and layout
    0% of the actual result

---
+++ js/utils.js
@@ -0,0 +1,416 @@
+//I like of these as globals
+const C$ = str => document.getElementById(str);
+const Sin = Math.sin
+const Cos = Math.cos
+
+const utils = (_ => {
+
+    const toRadFactor = Math.PI / 180
+    
+    Math.toRad = (v) => v * toRadFactor
+    Math.TWO_PI = Math.PI * 2
+    
+    const _canvas = document.createElement('canvas').getContext('2d')
+
+
+    const getRandRGB = () => {
+        _canvas.fillStyle = `hsl(${random(360)}, 100%, 50%)`
+        return hexToRgb(_canvas.fillStyle.slice(1))
+    }
+
+    const getRandHEX = () => {
+        _canvas.fillStyle = `hsl(${random(360)}, 100%, 50%)`
+        return _canvas.fillStyle
+    }
+
+    //By 'David'
+    //http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+    const hexToRgb = (hex) => {
+        const arrBuff = new ArrayBuffer(4);
+        const vw = new DataView(arrBuff);
+        vw.setUint32(0, parseInt(hex, 16), false);
+        const arrByte = new Uint8Array(arrBuff);
+        return [arrByte[1], arrByte[2], arrByte[3]]
+    }
+
+    let __;
+
+    const arrayOf = (n = 1, s = 0) => new Array(n).fill(1).map(i => new s())
+
+    const arrayBy = (n = 1, s = _=>0) => new Array(n).fill(1).map(i => s())
+
+    const range = (n, s = 0, f = 1) => new Array(n).fill(1).map(i => i = s += f)
+
+    const UUID = (a = 4, b = 4) => new Array(a).fill(0).map(i => new Array(b).fill(0).map(e => '0123456789abcdef'[Math.floor(Math.random() * 16)]).join('')).join('-')
+
+    const random = (v) => Math.floor(Math.random() * v)
+    
+
+    //By 'Francisc'
+    //http://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+    const randIntBetw = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
+
+    const randomOf = (obj, _) => Array.isArray(obj) ? obj[randIntBetw(0, obj.length - 1)] : (_ = Object.keys(obj), obj[_[randIntBetw(0, _.length)]])
+
+    const createCanvas = (w, h, auto = true) => {
+        const canvas = document.createElement('canvas')
+        if (!w) {
+            document.body.style.margin = '0px'
+            canvas.style.display = 'block'
+            w = canvas.height = window.innerHeight
+            h = canvas.width = window.innerWidth
+        } else {
+            canvas.height = h
+            canvas.width = w
+        }
+        auto && document.body.appendChild(canvas)
+        const ctx = canvas.getContext('2d')
+        ctx.clear = () => {
+            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+        }
+        ctx.circle = (x = 0, y = 0, r = 5) => {
+            ctx.beginPath()
+            ctx.ellipse(x, y, r, r, 0, 0, Math.TWO_PI)
+            ctx.fill()
+        }
+        return ctx
+    }
+
+    const getText = (url) => fetch(url, { mode: 'no-cors' }).then(r => r.text())
+
+    const getJSON = (url) => fetch(url, { mode: 'no-cors' }).then(r => r.json())
+
+    const getBlob = (url) => fetch(url, { mode: 'no-cors' }).then(r => r.blob())
+
+    const getArrayBuffer = (url) => fetch(url, { mode: 'no-cors' }).then(r => r.arrayBuffer())
+
+    //Copied from mozilla docs
+    //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+    const flatten = arr => arr.reduce((a, b) => a.concat(b))
+
+    //By 'jolly.exe'
+    //http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+    const getParameterByName = (name, url) => {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"), results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+
+    const utilData = {
+        textFormats: ['txt', 'glsl'],
+        imageFormats: ['png', 'bmp', 'gif', 'jpg', 'jpeg'],
+        videoFormats: ['mov', 'webm', 'mp4', 'swf'],
+        audioFormats: ['mp3', 'ogg', 'wav'],
+        parseableFormats: ['js', 'css', 'html'],
+        deniedFormats: [],
+    }
+
+    const loadScript = (url) => {
+        return new Promise((res, rej) => {
+            const script = document.createElement('script')
+            script.src = url
+            document.body.appendChild(script)
+            script.onerror = e => rej(e)
+            script.onload = e => res(e)
+        })
+    }
+
+    const pixelToGlCoord = (event, canvas) => {
+        var x = event.clientX, y = event.clientY;
+        var midX = canvas.width / 2, midY = canvas.height / 2;
+        var rect = event.target.getBoundingClientRect();
+        x = ((x - rect.left) - midX) / midX;
+        y = (midY - (y - rect.top)) / midY;
+        return { x, y }
+    }
+
+    const getE3ShaderData = (str) => {
+        let shaderData = { uniform: [], in: [] }
+        let data = str.match(/in.*;|uniform.*;/g)
+        data && data.map(item => {
+            let [input, type, name] = item.split(' ')
+            name = name.slice(0, name.length - 1)
+            shaderData[input].push(new ShaderDescriptor(name, type))
+        })
+        shaderData.isVertexShader = /gl_Position/.test(str)
+        return shaderData
+    }
+
+    const getE2ShaderData = (str) => {
+        let shaderData = { uniform: {}, attribute: {} }
+        let data = str.match(/attribute.*;|uniform.*;/g)
+        data && data.map(item => {
+            let [type1, type2, name] = item.split(' ')
+            name = name.slice(0, name.length - 1)
+            shaderData[type1][name] = {}
+            shaderData[type1][name].location = name
+            shaderData[type1][name].type = type2
+        })
+        shaderData.isFragmentShader = /gl_FragColor/.test(str)
+        shaderData.isVertexShader = /gl_Position/.test(str)
+        return shaderData
+    }
+
+    const loadAll = (obj = {}) => {
+        let resolved = {}
+        // iDontKnowHowToNameThis é um objeto que lembra os 'index' dos itens para 
+        //coloca-los de volta em um objeto igual o da 
+        //entrada e que sera usado para criar um outro objeto quando a Promisse for
+        //resolvida   
+        let iDontKnowHowToNameThis = {}
+        let promisseArray = []
+        let keys = Object.keys(obj)
+
+        for (let i of Object.keys(obj)) {
+            let path = obj[i]
+            let ext = getFileExtension(path)
+            if (isEqualToAny(ext, utilData.textFormats)) {
+                iDontKnowHowToNameThis[i] = promisseArray.push(getText(path)) - 1
+            } else if (isEqualToAny(ext, utilData.audioFormats)) {
+                iDontKnowHowToNameThis[i] = promisseArray.push(Url2Tag(path, 'audio')) - 1
+            } else if (isEqualToAny(ext, utilData.videoFormats)) {
+                iDontKnowHowToNameThis[i] = promisseArray.push(Url2Tag(path, 'video')) - 1
+            } else if (isEqualToAny(ext, utilData.imageFormats)) {
+                iDontKnowHowToNameThis[i] = promisseArray.push(Url2Tag(path, 'image')) - 1
+            } else if (isEqualToAny(ext, utilData.parseableFormats)) {
+                iDontKnowHowToNameThis[i] = promisseArray.push(Url2Tag(path, 'script')) - 1
+            } else if (isEqualToAny(ext, utilData.deniedFormats)) {
+                throw new Error('u got a virus m9.')
+            } else if (ext === 'json') {
+                iDontKnowHowToNameThis[i] = promisseArray.push(getJSON(path)) - 1
+            }
+        }
+        return Promise.all(promisseArray).then(data => {
+            for (let i of Object.keys(iDontKnowHowToNameThis)) {
+                resolved[i] = data[iDontKnowHowToNameThis[i]]
+            }
+            return resolved
+        })
+    }
+    //Modified version, by 'adnasa'
+    //https://gist.github.com/adnasa/94e26f50082454910657
+    const getFileExtension = (filePath = '') => {
+        const ext = filePath.split('.')
+        return ext[ext.length - 1]
+    }
+
+    const isEqualToAny = (input, items = []) => {
+        let res = false
+        items.map(i => !res && (res = i === input))
+        return res
+    }
+
+    const hasAllStrings = (input, strArr = []) => {
+        let res = true
+        strArr.map(val => !input.includes(val) && (res = false))
+        return res
+    }
+
+    const extensionToMediaType = (name) => {
+        if (isEqualToAny(name, utilData.videoFormats))
+            return 'video'
+        else if (isEqualToAny(name, utilData.audioFormats))
+            return 'audio'
+        else if (isEqualToAny(name, utilData.parseableFormats))
+            return 'script'
+        else if (isEqualToAny(name, utilData.imageFormats))
+            return 'image'
+        else return 'none'
+    }
+
+    //Magic
+    const Url2Tag = (url, type) => {
+        let result;
+        const ext = type || extensionToMediaType(getFileExtension(url))
+        switch (ext.toLowerCase()) {
+            case 'audio':
+                result = fetch(url, { mode: 'no-cors' }).then(r => r.blob().then(r => {
+                    let uri = URL.createObjectURL(r)
+                    let tag = document.createElement('audio')
+                    tag.src = uri
+                    return tag
+                }))
+                break;
+            case 'video':
+                result = fetch(url, { mode: 'no-cors' }).then(r => r.blob().then(r => {
+                    let uri = URL.createObjectURL(r)
+                    let tag = document.createElement('video')
+                    tag.src = uri
+                    return tag
+                }))
+                break;
+            case 'image':
+                result = fetch(url, { mode: 'no-cors' }).then(r => r.blob().then(r => {
+                    let uri = URL.createObjectURL(r)
+                    let tag = document.createElement('img')
+                    tag.src = uri
+                    return tag
+                }))
+                break;
+            case 'script':
+                result = fetch(url, { mode: 'no-cors' }).then(r => r.blob().then(r => {
+                    let uri = URL.createObjectURL(r)
+                    let tag = document.createElement('script')
+                    tag.src = uri
+                    return tag
+                }))
+                break;
+            default:
+                console.warn(`Unknow format ${ext}.`)
+                break;
+        }
+        return result;
+    }
+
+    /*Usage:
+    new KeyListener({
+        'KeyW':{
+            press(){player.jump()},
+            release(){ player.speed = 0; player.direction = -1}
+        }
+    } ,gl.canvas)*/
+    class KeyListener {
+        constructor(obj, element = window) {
+            element.addEventListener('keyup', this.keyUp.bind(this), false);
+            element.addEventListener('keydown', this.keyDown.bind(this), false);
+            this.elememt = element
+            this.keys = {}
+            for (let key in obj) {
+                this.keys[key] = {
+                    isDown: false,
+                    press: obj[key].press,
+                    release: obj[key].release
+                }
+            }
+        }
+        keyUp(e) {
+            if (this.keys[e.code] && this.keys[e.code].isDown) {
+                e.preventDefault()
+                this.keys[e.code].isDown = false
+                this.keys[e.code].release && this.keys[e.code].release()
+            }
+        }
+        keyDown(e) {
+            if (this.keys[e.code] && !this.keys[e.code].isDown) {
+                e.preventDefault()
+                this.keys[e.code].isDown = true
+                this.keys[e.code].press && this.keys[e.code].press()
+            }
+        }
+        detach() {
+            this.elememt.removeEventListener('keydown', this.keyDown, false)
+            this.elememt.removeEventListener('keyup', this.keyUp, false)
+        }
+    }
+
+    /*Usage
+    const keys = new BooleanicKeys({
+        'KeyW'(dt){player.x+=10*dt}
+    },gl.canvas) 
+    
+    let loop = (dt)=>{
+        //paramter is optional
+        keys.update(dt)
+    }
+     */
+    class BooleanicKeys {
+        constructor(obj, element = window) {
+            element.addEventListener('keyup', this.keyUp.bind(this), false);
+            element.addEventListener('keydown', this.keyDown.bind(this), false);
+            this.elememt = element
+            this.keys = {}
+            for (let key in obj) {
+                this.keys[key] = {
+                    isDown: false,
+                    press: obj[key],
+                }
+            }
+        }
+        keyUp(e) {
+            if (this.keys[e.code] && this.keys[e.code].isDown) {
+                e.preventDefault()
+                this.keys[e.code].isDown = false
+            }
+        }
+        keyDown(e) {
+            if (this.keys[e.code] && !this.keys[e.code].isDown) {
+                e.preventDefault()
+                this.keys[e.code].isDown = true
+            }
+        }
+        update(val) {
+            for (let key in this.keys) {
+                this.keys[key].isDown && this.keys[key].press(val)
+            }
+        }
+        detach() {
+            this.elememt.removeEventListener('keydown', this.keyDown, false)
+            this.elememt.removeEventListener('keyup', this.keyUp, false)
+        }
+    }
+
+    async function loadJSON() {
+        return await fetch('test.json').then(r => r.json())
+    }
+
+    let loop = fn => {
+        let lp = function(dt){
+            fn(dt)
+            requestAnimationFrame(lp)
+        }
+        lp()
+        return lp
+    } 
+
+    return Object.assign({
+        getRandRGB,
+        getRandHEX,
+        hexToRgb,
+        range,
+        UUID,
+        random,
+        randIntBetw,
+        createCanvas,
+        getText,
+        getJSON,
+        getBlob,
+        getArrayBuffer,
+        flatten,
+        getParameterByName,
+        loadScript,
+        pixelToGlCoord,
+        getE3ShaderData,
+        getE2ShaderData,
+        loadAll,
+        getFileExtension,
+        isEqualToAny,
+        hasAllStrings,
+        Url2Tag,
+        KeyListener,
+        BooleanicKeys,
+        loadJSON,
+        loop,
+        arrayOf,
+        arrayBy,
+        randomOf,
+    },{
+        //By 'Keith Peters' 2007 (colorized)
+        //https://github.com/bit101
+        
+        distanceXY: (x0, y0, x1, y1) => Math.sqrt(x1 - x0 ** 2 + y1 - y0 ** 2),
+        distance: (p0, p1) => Math.sqrt(p1.x - p0.x ** 2 + p1.y - p0.y ** 2),
+        norm: (value, min, max) => (value - min) / (max - min),
+        lerp: (norm, min, max) => (max - min) * norm + min,
+        map: (value, sourceMin, sourceMax, destMin, destMax) => utils.lerp(utils.norm(value, sourceMin, sourceMax), destMin, destMax),
+        clamp: (value, min, max) => Math.min(Math.max(value, Math.min(min, max)), Math.max(min, max)),
+        circleCollision: (c0, c1) => utils.distance(c0, c1) <= c0.radius + c1.radius,
+        circlePointCollision: (x, y, circle) => utils.distanceXY(x, y, circle.x, circle.y) < circle.radius,
+        pointInRect: (x, y, rect) => utils.inRange(x, rect.x, rect.x + rect.width) && utils.inRange(y, rect.y, rect.y + rect.height),
+        inRange: (value, min, max) => value >= Math.min(min, max) && value <= Math.max(min, max),
+        rangeIntersect: (min0, max0, min1, max1) => Math.max(min0, max0) >= Math.min(min1, max1) && Math.min(min0, max0) <= Math.max(min1, max1),
+        rectIntersect: (r0, r1) => utils.rangeIntersect(r0.x, r0.x + r0.width, r1.x, r1.x + r1.width) && utils.rangeIntersect(r0.y, r0.y + r0.height, r1.y, r1.y + r1.height),
+    })
+})()
---
+++ main.js
@@ -0,0 +1,74 @@
+const canvas = C$("canvas")
+
+onresize = _ => {
+    canvas.width = innerWidth
+    canvas.height = innerHeight
+}
+onresize()
+
+const ctx = canvas.getContext("2d");
+
+ctx.circle = (x = 0, y = 0, r = 5) => {
+    ctx.beginPath()
+    ctx.ellipse(x, y, r, r, 0, 0, Math.TWO_PI)
+    ctx.fill()
+}
+
+let ids = 1;
+
+class DotTicker {
+    constructor() {
+        this.x = utils.randIntBetw(50, innerWidth - 50)
+        this.y = utils.randIntBetw(50, innerHeight - 50)
+
+        this.radius = 500
+        this._radius = utils.randIntBetw(5, 20)
+
+        this.vx = utils.randIntBetw(-2, 2)
+        this.vy = utils.randIntBetw(-2, 2)
+
+        this.id = ids++
+    }
+
+    line() {
+        for (let dot of dots) {
+            ctx.moveTo(this.x, this.y)
+            ctx.lineTo(dot.x, dot.y)
+        }
+    }
+    tick() {
+        this.x += (this.x > innerWidth || this.x < 0 ? this.vx = -this.vx : this.vx)
+        this.y += (this.y > innerHeight || this.y < 0 ? this.vy = -this.vy : this.vy)
+        ctx.circle(this.x, this.y, this._radius)
+    }
+}
+
+const dots = utils.arrayOf(13, DotTicker)
+
+ctx.fillStyle = "#c0c0c0"
+ctx.strokeStyle = "rgba(30,30,30,0.3)"
+
+const s = new DotTicker()
+
+const rColor = utils.randomOf([
+    "#F3A94C","#FF2D80",
+    "#FFFC9B","#401063",
+    "#F870B1","#F42C29",
+    "#E6ECEF","#F4D53E",
+    "#8AE1FF","#0593E0",
+    "#D291EB","#6ADE51",
+])
+
+utils.loop(_ => {
+    ctx.fillStyle = "#c0c0c0"
+    ctx.clearRect(0, 0, innerWidth, innerHeight)
+    ctx.beginPath()
+    dots.map(i => i.line())
+    ctx.stroke()
+    dots.map(i => i.tick())
+    ctx.fillStyle = rColor
+    ctx.beginPath()
+    s.line()
+    ctx.stroke()
+    s.tick()
+})
